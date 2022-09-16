031bf2d3f96d60a84a2d6e1c5493c77840e589f3
sanyam99ag <sanyam99ag@gmail.com>
2022-09-17 03:05:39 +0530

    url update

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://shareany-.herokuapp.com";
+const baseURL = "https://shareanyy.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
